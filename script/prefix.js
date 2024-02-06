bb09e323181086c60fa8592a2273d0f06fb2c53e
kshitiz-008 <142625808+kshitiz-008@users.noreply.github.com>
2024-02-06 21:20:30 +0545

    Update prefix.js

--- script/prefix.js
+++ script/prefix.js
@@ -1,18 +1,18 @@
 module.exports = {
-    config: {
-        name: "prefix",
-        version: "1.0",
-        author: "kshitiz",
-        countDown: 1,
-        role: 0,
-        category: "know system prfix",
-    },
-    run: async function ({ api, event, message }) {
-        const triggerPhrases = ["prefix", "bot"];
-        if (event.body && triggerPhrases.includes(event.body.toLowerCase())) {
-            return () => {
-                return api.sendMessage("🌐 System prefix: ${prefix}\n🛸 Your box chat prefix: ${prefix}", event.threadID, event.messageID);
-            }
-        }
+  config: {
+    name: "prefix",
+    version: "1.0",
+    author: "kshitiz",
+    countDown: 5,
+    role: 0,
+    shortDescription: "no prefix",
+    longDescription: "no prefix",
+    category: "no prefix",
+  },
+  run: async function () {},
+  handleEvent: async function ({ api, event, message, getLang, prefix }) {
+    if (event.body && event.body.toLowerCase() === "prefix") {
+      return api.sendMessage(`🌐 System prefix: ${prefix}\n🛸 Your box chat prefix: ${prefix}`, event.threadID, event.messageID);
     }
+  }
 };
